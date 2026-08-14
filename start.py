33ef8521f64fa211fa8d584285068e583c5850ef
trefiks1 <mishakalshev2012@gmail.com>
2026-08-14 18:26:12 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import sys
+import uvicorn
 
 def main():
     port = int(os.getenv("PORT", 8080))
@@ -8,9 +9,8 @@ def main():
     # Запускаем бота в отдельном процессе
     bot_process = subprocess.Popen([sys.executable, "bot.py"])
     
-    # Запускаем FastAPI-сервер через uvicorn на порту Railway
-    import uvicorn
     try:
+        # Запускаем FastAPI сервер в главном потоке контейнера
         uvicorn.run("web_server:app", host="0.0.0.0", port=port)
     finally:
         bot_process.terminate()
